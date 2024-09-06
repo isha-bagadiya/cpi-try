06903e32fa740f366e17ad43896565977e165c06
jaydip24 <jaydip.lamprostech@gmail.com>
2024-09-06 10:22:03 +0530

    dynamically imported the component

--- components/home/FeaturedDAOChart.tsx
+++ components/home/FeaturedDAOChart.tsx
@@ -1,6 +1,11 @@
-import CPIChartForOP from "../common/CPIChartForOp";
-import CPIChartofDAOs from "../common/CPIChartofDAOs";
-import LineGraph from "../common/LineGraph";
+// import CPIChartForOP from "../common/CPIChartForOp";
+import dynamic from "next/dynamic";
+// import CPIChartofDAOs from "../common/CPIChartofDAOs";
+// import LineGraph from "../common/LineGraph";
+
+const CPIChartForOP = dynamic(() => import('../common/CPIChartForOp'), { ssr: false });
+const CPIChartofDAOs = dynamic(() => import('../common/CPIChartofDAOs'), { ssr: false });
+const LineGraph = dynamic(() => import('../common/LineGraph'), { ssr: false });
 
 const FeaturedDAOChart: React.FC = () => {
     return <>
