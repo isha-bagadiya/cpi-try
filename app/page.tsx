7fcf12312107e4bf3035b191878aba927739fac7
jaydip676 <jaydip.lamprostech@gmail.com>
2024-09-26 10:08:38 +0530

    enable smoothscrolling

--- app/page.tsx
+++ app/page.tsx
@@ -15,25 +15,25 @@ const Footer = dynamic(() => import('@/components/layout/Footer'), { ssr: false 
 
 export default function Home() {
   return (
-    // <SmoothScrolling>
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="w-[100%] overflow-x-hidden">
-        {/* <h1 className="bg-gradient-radial h-screen ">Hello <span className="text-[70px] border border-2 p-0 m-0">Nextjs</span> 14</h1>
+    <SmoothScrolling>
+      <main className="flex min-h-screen flex-col items-center">
+        <div className="w-[100%] overflow-x-hidden">
+          {/* <h1 className="bg-gradient-radial h-screen ">Hello <span className="text-[70px] border border-2 p-0 m-0">Nextjs</span> 14</h1>
         <button className="button-50" role="button">Button 50</button> */}
-        <Header />
-        <HeroSection />
-        <WhatIsCPI />
-        <Methodology />
-        <FeaturedDAOChart />
-        <ExpertAnalysis />
-        <div>
-          <Panels />
-        </div>
-        <AddYourDAO />
-        <Footer />
+          <Header />
+          <HeroSection />
+          <WhatIsCPI />
+          <Methodology />
+          <FeaturedDAOChart />
+          <ExpertAnalysis />
+          <div>
+            <Panels />
+          </div>
+          <AddYourDAO />
+          <Footer />
 
-      </div>
-    </main>
-    // </SmoothScrolling>
+        </div>
+      </main>
+    </SmoothScrolling>
   );
 }
