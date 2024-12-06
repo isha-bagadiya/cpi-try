4dfc6a9e0eccac66fbf099e708942794764c7d15
isha-bagadiya <ishabagadiya@lampos.tech>
2024-12-06 15:22:16 +0530

    formatted

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,4 +1,4 @@
-import DataTable from "@/components/common/DataTable";
+// import DataTable from "@/components/common/DataTable";
 import FilterButtons from "@/components/common/FilterButtons";
 import Header from "@/components/layout/Header";
 import { getItem, getItemCompound, getItemsAave, getItemsUniswap } from "@/lib/utils/fetchDataOnServer";
